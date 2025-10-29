3084ec8ce30ad203708ac4053fb6fb139229f446
Jonathan Gamon <jonygamon@gmail.com>
2025-10-29 19:10:39 -0300

    se agrego animacion al titulo de ocupacion y se disminuyo el tono de color de fondo en modo claro

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -11,3 +11,58 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.2 }); // 20% visible para activarse
 
 elementos.forEach(el => observer.observe(el));
+
+
+
+
+
+
+
+
+// animacion en subTitulo home
+const h1 = document.getElementById("hero-occupation-title");
+const NOMBRE = ["Desarrollador Front-End"];
+let A = [];
+let I = 0;
+const pausa = 1000;
+const velocidad = 100;
+
+const agregandoLetras = () => {
+    const agregar = setInterval(() => {
+        A.push(NOMBRE[0][I]);
+        I++;
+        h1.textContent = A.join('');
+
+
+        if (A.join('') == NOMBRE) {
+            clearInterval(agregar)
+
+            setTimeout(() => {
+                quitandoLetras()
+            }, pausa)
+            console.log("se detuvo")
+        }
+    }, velocidad)
+};
+
+
+const quitandoLetras = () => {
+    const quitar = setInterval(() => {
+        A.pop();
+        I--;
+        h1.textContent = A.join('');
+
+        if (A.length == 0) {
+            clearInterval(quitar);
+
+            setTimeout(() => {
+                agregandoLetras();
+            }, pausa)
+            console.log("listo")
+        }
+    }, velocidad)
+};
+
+setTimeout(()=> {
+    agregandoLetras()
+},500)
